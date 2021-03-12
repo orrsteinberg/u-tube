2969d0118cad800fc3c58112e4be21c69b16c913
Orr Steinberg <orr.sun10@gmail.com>
2021-03-12 19:03:57 +0200

    create basic theme

---
+++ src/utils/theme.js
@@ -0,0 +1,46 @@
+export const theme = {
+  sizes: {
+    headerHeight: "10vh",
+  },
+  colors: {
+    general: {
+      white: "#fff",
+      black: "#222222",
+      red: "#e9434d",
+      faded: "#5f6d8c",
+    },
+    text: {
+      primary: "#d8deed",
+      secondary: "#979fb4",
+      tertiary: "#707a9c",
+    },
+    bg: {
+      primary: "#101c36",
+      secondary: "#2a3658",
+      tertiary: "#223054",
+    },
+  },
+  typography: {
+    h1: "2.75rem",
+    h2: "2.25rem",
+    h3: "1.75rem",
+    h4: "1.5rem",
+    h5: "1.25rem",
+    h6: "1rem",
+  },
+  spacing: {
+    xxs: "0.25rem",
+    xs: "0.5rem",
+    sm: "0.75rem",
+    md: "1rem",
+    lg: "1.25rem",
+    xl: "1.5rem",
+    xxl: "1.75rem",
+  },
+  breakpoints: {
+    sm: "576px",
+    md: "768px",
+    lg: "992px",
+    xl: "1200px",
+  },
+};
