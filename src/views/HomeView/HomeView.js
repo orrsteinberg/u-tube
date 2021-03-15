92ec44002c3edf355096a9d62dbd7e719ce5b4af
Orr Steinberg <orr.sun10@gmail.com>
2021-03-15 18:19:38 +0200

    basic videos display on home view

--- src/views/HomeView/HomeView.js
+++ src/views/HomeView/HomeView.js
@@ -1,8 +1,23 @@
 import React from "react";
 import styled from "styled-components";
 
-const HomeView = ({}) => {
-  return <div>Home View</div>;
+import Video from "../../components/Video/Video";
+
+const Row = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const HomeView = () => {
+  return (
+    <Row>
+      {[...Array(20)].map((x, i) => (
+        <Video key={i} />
+      ))}
+    </Row>
+  );
 };
 
 export default HomeView;
