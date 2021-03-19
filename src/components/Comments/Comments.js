dedaf2f0417af225efb3baa72a403513a64a2c29
Orr Steinberg <orr.sun10@gmail.com>
2021-03-20 00:23:39 +0200

    add new comment form

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,5 +1,75 @@
 import React from "react";
+import styled from "styled-components";
 
-const Comments = () => <div>Comments</div>;
+const NewComment = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 1rem;
+
+  img {
+    border-radius: 50%;
+    width: 50px;
+    object-fit: contain;
+  }
+
+  form {
+    display: flex;
+    flex: 1;
+
+    input {
+      flex: 1;
+      border: none;
+      background: none;
+      border-bottom: 3px solid ${({ theme }) => theme.colors.bg.secondary};
+      margin-left: 1rem;
+      margin-right: 1rem;
+      color: ${({ theme }) => theme.colors.text.primary};
+      padding: 0.25rem 0;
+      font-size: 1.1rem;
+      transition: border-bottom 0.2s;
+
+      &::placeholder {
+        color: ${({ theme }) => theme.colors.text.secondary};
+      }
+
+      &:focus {
+        outline: none;
+        border-bottom: 3px solid ${({ theme }) => theme.colors.text.primary};
+      }
+    }
+
+    button {
+      border: none;
+      background: ${({ theme }) => theme.colors.general.faded};
+      color: ${({ theme }) => theme.colors.general.light};
+      cursor: pointer;
+      text-transform: uppercase;
+      padding: 0.5rem 1rem;
+
+      &:focus {
+        outline: none;
+      }
+    }
+  }
+`;
+
+const CommentsContainer = styled.div`
+  width: 100%;
+`;
+
+const Comments = () => {
+  return (
+    <CommentsContainer>
+      <h2>3,692 Comments</h2>
+      <NewComment>
+        <img src="https://picsum.photos/40" alt="avatar" />
+        <form>
+          <input type="text" placeholder="Add a comment..." />
+          <button type="submit">Comment</button>
+        </form>
+      </NewComment>
+    </CommentsContainer>
+  );
+};
 
 export default Comments;
