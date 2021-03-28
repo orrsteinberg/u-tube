e4415e4474aa3a6f3fc7778b964de99e3a4e4ada
Orr Steinberg <orr.sun10@gmail.com>
2021-03-29 00:15:42 +0300

    add helper function for parsing url query strings

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,5 +1,21 @@
+// Truncate text
 export const truncateTitle = (title) =>
   title.length > 70 ? title.substr(0, 67) + "..." : title;
 
 export const truncateDescription = (description) =>
   description.length > 150 ? description.substr(0, 147) + "..." : description;
+
+// Parse query strings
+export const getQueryStringParams = (query) => {
+  return query
+    ? (/^[?#]/.test(query) ? query.slice(1) : query)
+        .split("&")
+        .reduce((params, param) => {
+          let [key, value] = param.split("=");
+          params[key] = value
+            ? decodeURIComponent(value.replace(/\+/g, " "))
+            : "";
+          return params;
+        }, {})
+    : {};
+};
