27763fa9153fb09656da40bcd1a9d017e9a91712
Orr Steinberg <orr.sun10@gmail.com>
2021-03-28 17:18:33 +0300

    add truncateDescription helper function

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,1 +1,5 @@
-export const truncateTitle = (title) => title.length > 70 ? title.substr(0, 67) + "..." : title;
+export const truncateTitle = (title) =>
+  title.length > 70 ? title.substr(0, 67) + "..." : title;
+
+export const truncateDescription = (description) =>
+  description.length > 150 ? description.substr(0, 147) + "..." : description;
