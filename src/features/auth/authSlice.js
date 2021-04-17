fcb20cac326551b3bc5e7a09dfdef5176393fbdd
Orr Steinberg <orr.sun10@gmail.com>
2021-04-17 21:56:20 +0300

    temp login/logout functionality

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -13,14 +13,20 @@ const initialState = {
 
 // Async thunks
 export const login = createAsyncThunk("auth/login", async () => {
-  const provider = new firebase.auth.GoogleAuthProvider();
-  provider.addScope("https://www.googleapis.com/auth/youtube.force-ssl");
+  const provider = new firebase.auth.GoogleAuthProvider().addScope(
+    "https://www.googleapis.com/auth/youtube.force-ssl"
+  );
 
   const authResponse = await auth.signInWithPopup(provider);
-  return {
-    credential: authResponse.credential,
-    profile: authResponse.additionalUserInfo.profile,
-  };
+
+  const { accessToken } = authResponse.credential;
+  const { displayName, photoURL } = authResponse.user;
+
+  return { accessToken, displayName, photoURL };
+});
+
+export const logout = createAsyncThunk("auth/logout", async () => {
+  await auth.signOut();
 });
 
 // State slice
@@ -36,17 +42,23 @@ const authSlice = createSlice({
     [login.fulfilled]: (state, action) => {
       state.status = "succeeded";
 
-      const { credential, profile } = action.payload;
+      const { accessToken, displayName, photoURL } = action.payload;
 
-      state.accessToken = credential.accessToken;
+      state.accessToken = accessToken;
       state.user = {
-        name: profile.name,
-        avatar: profile.picture,
+        name: displayName,
+        avatar: photoURL,
       };
     },
     [login.rejected]: (state, action) => {
       state.status = "failed";
       state.error = action.error.message;
+    },
+    [logout.fulfilled]: (state, action) => {
+      state.status = "idle";
+      state.error = null;
+      state.accessToken = null;
+      state.user = null;
     },
   },
 });
