c5dde77c0af3d21137d4c04ed3c56956e8d6b4dd
Orr Steinberg <orr.sun10@gmail.com>
2021-04-19 15:42:19 +0300

    add subscribe button component

---
+++ src/components/SubscribeButton/SubscribeButton.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { selectUser } from "../../features/auth/authSlice";
+import { StyledSubscribeButton } from "./SubscribeButton.styled";
+
+import {
+  selectSubscriptions,
+  updateSubscription,
+} from "../../features/subscriptions/subscriptionsSlice";
+
+const SubscribeButton = ({ channelId }) => {
+  const [isSubscribed, setIsSubscribed] = useState(false);
+  const subscriptions = useSelector(selectSubscriptions);
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // Check subscription status on user/subscriptions state change
+    if (user) {
+      const hasSubscription = subscriptions.items.find(
+        (item) => item.channel.id === channelId
+      );
+      setIsSubscribed(hasSubscription);
+    }
+  }, [user, subscriptions, channelId]);
+
+  const handleClick = () => {
+    dispatch(updateSubscription(channelId));
+  };
+
+  const text = isSubscribed ? "Subscribed" : "Subscribe";
+
+  return (
+    <StyledSubscribeButton isSubscribed={isSubscribed} onClick={handleClick}>
+      {text}
+    </StyledSubscribeButton>
+  );
+};
+
+export default SubscribeButton;
---
+++ src/components/SubscribeButton/SubscribeButton.styled.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+import { lighten } from "polished";
+
+export const StyledSubscribeButton = styled.button`
+  border: 0;
+  border-radius: 3px;
+  background: ${({ isSubscribed, theme }) =>
+    isSubscribed ? theme.colors.general.faded : theme.colors.general.red};
+  color: ${({ theme }) => theme.colors.general.light};
+  font-size: 1rem;
+  text-transform: uppercase;
+  padding: 0.75rem 1rem;
+  line-height: 1;
+  cursor: pointer;
+
+  &:active {
+    background: ${({ isSubscribed, theme }) =>
+      isSubscribed
+        ? lighten(0.05, theme.colors.general.faded)
+        : lighten(0.05, theme.colors.general.red)};
+  }
+`;
