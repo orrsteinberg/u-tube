7ec2ec1612d67c177549fed98b85e73f6f5cb14a
Orr Steinberg <orr.sun10@gmail.com>
2021-04-24 16:24:15 +0300

    use forwardRef with nav element, add avatar highlights

--- src/components/Sidemenu/Sidemenu.js
+++ src/components/Sidemenu/Sidemenu.js
@@ -21,6 +21,7 @@ import {
   AuthButton,
   ViewAllSubsButton,
 } from "./Sidemenu.styled";
+import { truncateSubscriptionTitle } from "../../utils/helpers";
 import { selectUser } from "../../features/auth/authSlice";
 import { selectSubscriptions } from "../../features/subscriptions/subscriptionsSlice";
 import Avatar from "../Avatar/Avatar";
@@ -42,16 +43,20 @@ const SubscribedChannelsGroup = () => {
     ) : null;
 
   return (
-    <NavGroup>
+    <NavGroup className="hideOnMobile">
       {listToDisplay.map((item) => (
         <Link to={`/channel/${item.channel.id}`} key={item.channel.id}>
           <NavItem>
             <Avatar
+              highlight={item.channel.newVideoCount > 0}
               size="xs"
               src={item.channel.avatar}
               alt={item.channel.title}
             />{" "}
-            <span>{item.channel.title}</span>
+            <span>{truncateSubscriptionTitle(item.channel.title)}</span>
+            {item.channel.newVideoCount > 0 && (
+              <span className="circleHighlight">●</span>
+            )}
           </NavItem>
         </Link>
       ))}
@@ -60,56 +65,58 @@ const SubscribedChannelsGroup = () => {
   );
 };
 
-const Sidemenu = ({ showOnMobile, activeTab, watchView }) => {
-  const [signIn, signOut] = useGoogleAuth();
-  const user = useSelector(selectUser);
+const Sidemenu = React.forwardRef(
+  ({ showOnMobile, activeTab, watchView }, ref) => {
+    const [signIn, signOut] = useGoogleAuth();
+    const user = useSelector(selectUser);
 
-  const handleAuth = () => {
-    user ? signOut() : signIn();
-  };
+    const handleAuth = () => {
+      user ? signOut() : signIn();
+    };
 
-  const authIcon = user ? <IoMdLogOut /> : <MdPerson />;
-  const authText = user ? "Sign out" : "Sign in";
+    const authIcon = user ? <IoMdLogOut /> : <MdPerson />;
+    const authText = user ? "Sign out" : "Sign in";
 
-  return (
-    <Nav showOnMobile={showOnMobile} watchView={watchView}>
-      <NavGroup>
-        <Link to="/">
-          <NavItem active={activeTab === "home"}>
-            <MdHome /> <span>Home</span>
+    return (
+      <Nav showOnMobile={showOnMobile} watchView={watchView} ref={ref}>
+        <NavGroup>
+          <Link to="/">
+            <NavItem active={activeTab === "home"}>
+              <MdHome /> <span>Home</span>
+            </NavItem>
+          </Link>
+          <Link to="/explore">
+            <NavItem active={activeTab === "explore"}>
+              <MdExplore /> <span>Explore</span>
+            </NavItem>
+          </Link>
+          <Link to="/subscriptions">
+            <NavItem active={activeTab === "subscriptions"}>
+              <MdSubscriptions /> <span>Subscriptions</span>
+            </NavItem>
+          </Link>
+        </NavGroup>
+        <NavGroup>
+          <NavItem disabled>
+            <MdVideoLibrary /> <span>Library</span>
           </NavItem>
-        </Link>
-        <Link to="/explore">
-          <NavItem active={activeTab === "explore"}>
-            <MdExplore /> <span>Explore</span>
+          <NavItem disabled>
+            <MdHistory /> <span>History</span>
           </NavItem>
-        </Link>
-        <Link to="/subscriptions">
-          <NavItem active={activeTab === "subscriptions"}>
-            <MdSubscriptions /> <span>Subscriptions</span>
+          <NavItem disabled>
+            <MdVideocam /> <span>My Videos</span>
           </NavItem>
-        </Link>
-      </NavGroup>
-      <NavGroup>
-        <NavItem disabled>
-          <MdVideoLibrary /> <span>Library</span>
-        </NavItem>
-        <NavItem disabled>
-          <MdHistory /> <span>History</span>
-        </NavItem>
-        <NavItem disabled>
-          <MdVideocam /> <span>My Videos</span>
-        </NavItem>
-        <NavItem disabled>
-          <MdWatchLater /> <span>Watch Later</span>
-        </NavItem>
-      </NavGroup>
-      {user && <SubscribedChannelsGroup />}
-      <AuthButton as="button" onClick={handleAuth}>
-        {authIcon} <span>{authText}</span>
-      </AuthButton>
-    </Nav>
-  );
-};
+          <NavItem disabled>
+            <MdWatchLater /> <span>Watch Later</span>
+          </NavItem>
+        </NavGroup>
+        {user && <SubscribedChannelsGroup />}
+        <AuthButton as="button" onClick={handleAuth}>
+          {authIcon} <span>{authText}</span>
+        </AuthButton>
+      </Nav>
+    );
+  }
+);
 
 export default Sidemenu;
