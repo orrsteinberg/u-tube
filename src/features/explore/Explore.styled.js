79edf27c345876247b71904e11d55ff3d401211f
Orr Steinberg <orr.sun10@gmail.com>
2021-04-24 16:14:45 +0300

    tweak mobile styles

--- src/features/explore/Explore.styled.js
+++ src/features/explore/Explore.styled.js
@@ -68,7 +68,11 @@ export const ExploreText = styled.h3`
   display: block;
   margin-top: 2rem;
   text-align: center;
-  font-size: 1.75rem;
+  font-size: 1.5rem;
   font-weight: normal;
   color: ${({ theme }) => theme.colors.text.tertiary};
+
+  @media screen and (min-width: ${({ theme }) => theme.breakpoints.md}) {
+    font-size: 1.75rem;
+  }
 `;
