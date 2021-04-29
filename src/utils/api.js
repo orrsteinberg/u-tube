ea0661cd1ec8999f2523dc4c23a044ff6a2107ae
Orr Steinberg <orr.sun10@gmail.com>
2021-04-29 21:19:01 +0300

    change url to use an api proxy

--- src/utils/api.js
+++ src/utils/api.js
@@ -9,10 +9,7 @@ import {
 } from "./constants";
 
 const request = axios.create({
-  baseURL: "https://youtube.googleapis.com/youtube/v3/",
-  params: {
-    key: process.env.REACT_APP_YT_API_KEY,
-  },
+  baseURL: process.env.REACT_APP_API_PROXY,
 });
 
 const api = {
