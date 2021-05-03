006a531c33c6b3102cd978ff61b36f89cec6d5db
Orr Steinberg <orr.sun10@gmail.com>
2021-05-03 19:28:20 +0300

    scroll to top when changing views

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useLayoutEffect, useRef } from "react";
 
 import { useDetectPageClick } from "../../hooks";
 import Header from "../Header/Header";
@@ -8,9 +8,15 @@ import { Main, ViewContainer } from "./Layout.styled";
 const Layout = ({ children, activeTab, watchView }) => {
   const menuRef = useRef(null);
   const [showMenuOnMobile, setShowMenuOnMobile] = useDetectPageClick(
-    menuRef,
-    false
+    menuRef, // Element
+    false // Initial value
   );
+  const viewContainerRef = useRef(null);
+
+  useLayoutEffect(() => {
+    // Scroll to top when changing views
+    viewContainerRef.current.scrollTop = 0;
+  });
 
   const handleMenuToggle = () => setShowMenuOnMobile(!showMenuOnMobile);
 
@@ -24,7 +30,11 @@ const Layout = ({ children, activeTab, watchView }) => {
           activeTab={activeTab}
           watchView={watchView}
         />
-        <ViewContainer id="view-container" menuIsOpen={showMenuOnMobile}>
+        <ViewContainer
+          ref={viewContainerRef}
+          id="view-container"
+          menuIsOpen={showMenuOnMobile}
+        >
           {children}
         </ViewContainer>
       </Main>
