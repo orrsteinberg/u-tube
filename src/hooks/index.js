3e9da16aa0072093cfb35a118f0215407824359f
Orr Steinberg <orr.sun10@gmail.com>
2021-05-07 18:57:37 +0300

    add useTextExpand custom hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,2 +1,3 @@
 export { default as useGoogleAuth } from "./useGoogleAuth";
 export { default as useDetectPageClick } from "./useDetectPageClick";
+export { default as useTextExpand } from "./useTextExpand";
