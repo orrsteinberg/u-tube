2d1d7119ae353c8b69f0e90a9efba3c2f349afb7
Orr Steinberg <orr.sun10@gmail.com>
2021-05-11 20:31:13 +0300

    add active state to subscription when viewing the page

--- src/components/Sidemenu/SubscriptionsNavGroup.js
+++ src/components/Sidemenu/SubscriptionsNavGroup.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useRouteMatch } from "react-router-dom";
 
 import { NavGroup, NavItem } from "./Sidemenu.styled";
 import { ViewAllSubsButton } from "./SubscriptionsNavGroup.styled";
@@ -12,14 +12,19 @@ const SubscriptionsNavGroup = () => {
   // Show only the first 5 subscriptions, expand list to show the rest
   const [isExpanded, setIsExpanded] = useState(false);
   const { items: fullList } = useSelector(selectSubscriptions);
+  const { path, params } = useRouteMatch();
+
+  // Add active state to nav item if user is viewing that channel
+  const viewingChannel = path.startsWith("/channel");
 
   const listToDisplay = isExpanded ? fullList : fullList.slice(0, 5);
 
-  const handleToggle = () => setIsExpanded(!isExpanded);
+  const toggleList = () => setIsExpanded(!isExpanded);
 
+  // Only display button if user has more than 5 subscriptions
   const button =
     fullList.length > 5 ? (
-      <ViewAllSubsButton onClick={handleToggle}>
+      <ViewAllSubsButton onClick={toggleList}>
         {isExpanded ? "show less" : "show all"}
       </ViewAllSubsButton>
     ) : null;
@@ -28,7 +33,7 @@ const SubscriptionsNavGroup = () => {
     <NavGroup className="hideOnMobile">
       {listToDisplay.map((item) => (
         <Link to={`/channel/${item.channel.id}`} key={item.channel.id}>
-          <NavItem>
+          <NavItem active={viewingChannel && params.id === item.channel.id}>
             <Avatar
               highlight={item.channel.newVideoCount > 0}
               size="xs"
